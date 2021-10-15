fabaf200e00d936ef66015323d249f05970e017a
听泉么么哒 <fm98.7@qq.com>
2021-10-15 17:30:07 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -41,7 +41,7 @@ Sub_info = script-name=Sub_info,update-interval=600
   }
   if (expire) {
     if (/^[\d]+$/.test(expire)) expire *= 1000;
-    content.push(`Expire：${formatTime(expire)}`);
+    content.push(`Expired：${formatTime(expire)}`);
   }
   
   let now = new Date();
